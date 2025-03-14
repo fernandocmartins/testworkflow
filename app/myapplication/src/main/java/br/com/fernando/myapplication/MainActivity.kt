e682265f0de31e853d7b8bb6c36818482de06b73
fernandocmartins <40214510+fernandocmartins@users.noreply.github.com>
2025-03-14 20:27:12 -0300

    more tests (#15)

--- app/myapplication/src/main/java/br/com/fernando/myapplication/MainActivity.kt
+++ app/myapplication/src/main/java/br/com/fernando/myapplication/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             TestworkflowTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Frio é bom, de verdade, verdadeira, mermo",
+                        name = "Frio é bom, de verdade, verdadeira, mermo, sumemo",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
