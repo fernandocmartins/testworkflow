52eab11d0bdbbbc29526c34b2d3b602a80bb2fc1
Fernando <fernandocsrmartins@gmail.com>
2025-03-14 19:59:49 -0300

    teste vlalb

--- app/myapplication/src/main/java/br/com/fernando/myapplication/MainActivity.kt
+++ app/myapplication/src/main/java/br/com/fernando/myapplication/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             TestworkflowTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Bluves",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
